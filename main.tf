fd6b8c65337ba78a3a285cd255dd9f273b17d4f9
santosh-eic <santosh.pagire@einfochips.com>
2024-12-24 17:42:32 +0530

    source changed

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "ec2" {
-    source = "git@github.com:santoshPagire/terrafrom_ec2.git"
+    source = "https://github.com/santoshPagire/terrafrom_ec2.git"
     ami = var.ami
     instance_type = var.instance_type 
 }
 
 module "s3" {
-    source = "git@github.com:santoshPagire/terraform_s3.git"
+    source = "https://github.com/santoshPagire/terraform_s3.git"
     bucket_name = var.bucket_name
 }
